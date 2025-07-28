416a760e77e690fc663203bb40fdffd947015ee9
iamsameernajam <87220036+iamsameernajam@users.noreply.github.com>
2025-07-28 08:40:03 +0530

    Time: 0 ms (100%), Space: 41.6 MB (43.52%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count=0;
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                count++;
+            }        
+            else if(s.charAt(i)==')'){
+                ans=Math.max(ans,count);
+                count--;
+            }
+        }
+        ans=Math.max(ans,count);
+        return ans;
+    }
+}
