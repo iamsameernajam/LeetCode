10fd6b1d56d2e5410a748a9cd059a0a963ab56b1
iamsameernajam <87220036+iamsameernajam@users.noreply.github.com>
2025-07-28 09:05:30 +0530

    Time: 60 ms (39.91%), Space: 42.5 MB (96.19%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int beautySum(String s) {
+        int n = s.length(),sum=0;
+        for(int i=0;i<n;i++) {
+            int[] freq = new int[26];
+            for(int j=i;j<n;j++) {
+                freq[s.charAt(j)-'a']++;
+                int max = Integer.MIN_VALUE;
+                int min = Integer.MAX_VALUE;
+                for(int k=0;k<26;k++) {
+                    if(freq[k]>0) {
+                        max = Math.max(max, freq[k]);
+                        min = Math.min(min, freq[k]);
+                    }
+                }
+                sum+=max-min;
+            }
+        }
+        return sum;
+    }
+}
