f4ddc2abe8cd6810b87627246db20f9593b3d709
iamsameernajam <87220036+iamsameernajam@users.noreply.github.com>
2025-08-06 09:06:57 +0530

    Time: 4 ms (2.27%), Space: 40.6 MB (37.93%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.java
+++ 1086-divisor-game/1086-divisor-game.java
@@ -1,5 +1,19 @@
-class Solution {
+public class Solution {
     public boolean divisorGame(int n) {
-        return n%2==0;
+        boolean[] dp = new boolean[n + 1];
+
+        dp[0] = false; 
+        dp[1] = false;
+
+        for (int i = 2; i <= n; i++) {
+            for (int x = 1; x < i; x++) {
+                if (i % x == 0 && dp[i - x] == false) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
     }
 }
