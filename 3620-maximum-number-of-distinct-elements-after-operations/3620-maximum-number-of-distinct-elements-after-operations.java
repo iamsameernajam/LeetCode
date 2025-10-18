22e27bdc082f722671db7e7ceaf78121ea545b86
iamsameernajam <87220036+iamsameernajam@users.noreply.github.com>
2025-10-18 09:41:22 +0530

    Time: 20 ms (47.59%), Space: 58.1 MB (73.79%) - LeetHub

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxDistinctElements(int[] nums, int k) {
+        int ans = 0;
+        int occupied = Integer.MIN_VALUE;
+        
+        Arrays.sort(nums);
+        
+        for (int num : nums) {
+            if (occupied < num + k) {
+                int newAssigned = Math.max(occupied + 1, num - k);
+                
+                occupied = newAssigned;
+                ans++;
+            }
+           
+        }
+        
+        return ans;
+    }
+}
